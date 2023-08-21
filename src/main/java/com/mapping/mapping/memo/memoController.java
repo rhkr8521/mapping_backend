7071cd458711fb6a8c9d26bcaa46a36091a90b20
rhkr8521 <rhkr8521@gmail.com>
2023-08-21 10:22:49 +0900

    Update Writer, Tag Search Feature

--- src/main/java/com/mapping/mapping/memo/memoController.java
+++ src/main/java/com/mapping/mapping/memo/memoController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @RestController
 @RequestMapping(value = "/memo")
 public class memoController {
@@ -29,29 +31,54 @@ public class memoController {
 		return boardRep.save(new memo(content, writer, lat, log, img, date, tag));
 	}
 	
-    //테이블 리스트 가져오기
+    //메모 전체 로드 : {url}/memo
 	@GetMapping
 	public Iterable<memo> list(){
 		return boardRep.findAll();
 	}
 	
-    //id로 테이블 값 가져오기
+    //ID검색 : {url}/memo/{id}
 	@GetMapping(value = "/{id}")
 	public Optional<memo> findOne(@PathVariable Long id) {
 		return boardRep.findById(id);
 	}
 
+	//작성자검색 : {url}/memo/search?writer={writer}
+	@GetMapping(value = "/search")
+	public List<memo> searchByWriter(@RequestParam String writer) {
+    	return boardRep.findByWriter(writer);
+	}
+
+	//태그검색 : {url}/memo/tagsearch?tag={tag}
+	@GetMapping(value = "/tagsearch")
+	public List<memo> searchByTag(@RequestParam String tag){
+		return boardRep.findByTag(tag);
+	}
+
     //id로 테이블 값 수정
 	@PutMapping(value = "/{id}")
-	public memo update(@PathVariable Long id, @RequestParam String writer) {
-		Optional<memo> board = boardRep.findById(id);
-		board.get().setWriter(writer);
-		return boardRep.save(board.get());
-}
+	public memo update(@PathVariable Long id, @RequestParam String content, @RequestParam String writer, @RequestParam String lat, @RequestParam String log, @RequestParam String img, @RequestParam String date, @RequestParam String tag) {
+    	Optional<memo> board = boardRep.findById(id);
+    	if (board.isPresent()) {
+        	memo memoToUpdate = board.get();
+        	memoToUpdate.setContent(content);
+        	memoToUpdate.setWriter(writer);
+        	memoToUpdate.setLat(lat);
+        	memoToUpdate.setLog(log);
+        	memoToUpdate.setImg(img);
+        	memoToUpdate.setDate(date);
+        	memoToUpdate.setTag(tag);
+        	return boardRep.save(memoToUpdate);
+    	} else {
+        	throw new EntityNotFoundException("Memo with id " + id + " not found");
+    	}
+	}
+
 	
     //id로 테이블 값 삭제
-	@DeleteMapping
-	public void delete(@RequestParam Long id) {
-		boardRep.deleteById(id);
+	@DeleteMapping(value = "/delete")
+	public void deleteById(@RequestParam(value = "id") Long id) {
+    	boardRep.deleteById(id);
 	}
+
 }
