8d2389b132baad8368e0ae13adcabead1cfc32a4
rhkr8521 <rhkr8521@gmail.com>
2023-08-25 21:41:57 +0900

    Add token auth create, delete, search memo

--- src/main/java/com/mapping/mapping/user/controller/MainController.java
+++ src/main/java/com/mapping/mapping/user/controller/MainController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin(originPatterns = "http://localhost:3000")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping("")
     public String hello(){
-        return "good";
+        return "hello Mapping";
     }
 
 
