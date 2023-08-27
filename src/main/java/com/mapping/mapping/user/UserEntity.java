f56caf8572c3d834ff2dbeac4fe762c7c221991b
keviness0720 <hsoo0720@naver.com>
2023-08-27 12:12:30 +0900

    Update receive only Email When signing up

--- src/main/java/com/mapping/mapping/user/UserEntity.java
+++ src/main/java/com/mapping/mapping/user/UserEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Table(name="User") //DB에 있는 해당 테이블과 현재 클래스를 매핑
 public class UserEntity {
     @Id
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String userEmail;
     @NotEmpty(message = "사용자 Password는 필수항목입니다.")
     private String userPassword;
--- src/main/java/com/mapping/mapping/user/dto/SignUpDto.java
+++ src/main/java/com/mapping/mapping/user/dto/SignUpDto.java
@@ -1,5 +1,6 @@
 package com.mapping.mapping.user.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpDto {
+
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String userEmail;
+
     private String userPassword;
+
     private String userPasswordcheck;
+
     private String userNickname;
 
 }
